53eb1845d694719a53270651edd7a114624784cc
Anilathmacloudeqs <129284639+Anilathmacloudeqs@users.noreply.github.com>
2023-11-13 12:23:54 +0530

    Add files via upload

---
+++ .github/workflows/getcommitid.ps1
@@ -0,0 +1,23 @@
+param (
+    $SourceTag
+)
+git config --global user.name 'Anilathmacloudeqs' 
+git config --global user.email 'anilathma@cloudeqs.com'
+git config pull.rebase false 
+git config --list
+git branch
+Write-Host "testing the git tag list"
+git tag
+Write-Host "testing after git tag"
+Write-Host "##change Executing git pull on $($SourceTag)"
+git pull origin $SourceTag --allow-unrelated-histories
+#git checkout $SourceTag
+
+Write-Host "getting commit id for $($SourceTag)"
+git log -n 1 
+
+$commitid = (git log -n 1 )[0].split(" ")[-1]
+
+Write-Host "commit id is $($commitid)"
+
+echo "COMMIT_ID=$commitid" >> $env:GITHUB_ENV
