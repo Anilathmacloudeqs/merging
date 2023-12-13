6fd58d6366d64bdedeb0a3455a34c523af5d0370
Anilathmacloudeqs <129284639+Anilathmacloudeqs@users.noreply.github.com>
2023-12-13 18:52:36 +0530

    Update push-to-release.ps1

--- push-to-release.ps1
+++ push-to-release.ps1
@@ -18,6 +18,16 @@ git checkout -B release
 git add .
 git commit -m "Update release branch"
 
+# Navigate to the repository root
+cd $PSScriptRoot
+
+# Create a new file or modify an existing one
+echo "Hello, Release Branch!" > release_file.txt
+
+# Add and commit the changes
+git add .
+git commit -m "Update release branch"
+
 # Push changes to the release branch using a personal access token (replace YOUR_TOKEN and YOUR_USERNAME)
 $token = "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"
 $githubUsername = "YOUR_USERNAME"
