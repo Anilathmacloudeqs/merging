028c744018b1f8c2b875e12bd626e7d5845f632a
Anilathmacloudeqs <129284639+Anilathmacloudeqs@users.noreply.github.com>
2023-12-13 18:33:16 +0530

    Update push-to-release.ps1

--- push-to-release.ps1
+++ push-to-release.ps1
@@ -1,8 +1,18 @@
 # push-to-release.ps1
 
-# Run the hello.py script
+# Run the hello.py script (if needed)
 $pythonScriptPath = ".\hello.py"
 $result = & python $pythonScriptPath
 
-# Print the result
-Write-Host "Result from hello.py script: $result"
+# Navigate to the repository root
+cd $PSScriptRoot
+
+# Checkout the release branch or create it if it doesn't exist
+git checkout -B release
+
+# Add and commit changes
+git add .
+git commit -m "Update release branch"
+
+# Push changes to the release branch
+git push origin release
