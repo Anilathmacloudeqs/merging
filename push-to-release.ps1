1c69368e765d81aa1515fbb77907eb9a44ef0366
Anilathmacloudeqs <129284639+Anilathmacloudeqs@users.noreply.github.com>
2023-12-13 14:01:11 +0530

    Update push-to-release.ps1

--- push-to-release.ps1
+++ push-to-release.ps1
@@ -8,7 +8,7 @@ git config --global user.email "actions@github.com"
 git config --global user.name "GitHub Actions"
 
 # Fetch the release branch (create it if it doesn't exist)
-git fetch origin $destinationBranch:$destinationBranch
+git fetch origin "$($destinationBranch):$($destinationBranch)"
 
 # Checkout the release branch
 git checkout $destinationBranch
