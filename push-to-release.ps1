2045e33e56cdb515c2d39a318954e208a8199563
Anilathmacloudeqs <129284639+Anilathmacloudeqs@users.noreply.github.com>
2023-12-13 13:57:21 +0530

    Update push-to-release.ps1

--- push-to-release.ps1
+++ push-to-release.ps1
@@ -3,22 +3,16 @@ $sourceBranch = "main"
 $destinationBranch = "release"
 $fileToPush = "hello.py"
 
-# Debug statements
-Write-Host "Current directory: $(Get-Location)"
-Write-Host "Files in main branch: $(git ls-tree $sourceBranch)"
+# Set Git user information
+git config --global user.email "actions@github.com"
+git config --global user.name "GitHub Actions"
 
 # Copy the file from main branch to release branch
 git checkout $sourceBranch -- $fileToPush
 
-# Debug statements
-Write-Host "Files after checkout: $(git ls-files)"
-
 # Stage and commit changes
 git add $fileToPush
 git commit -m "Update $fileToPush from $sourceBranch to $destinationBranch"
-
-# Debug statements
-Write-Host "Files after commit: $(git ls-files)"
 
 # Push changes to the release branch
 git push origin $destinationBranch
