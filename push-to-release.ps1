23319226085a94a04a6b7db4ab93cc3eafa6a023
Anilathmacloudeqs <129284639+Anilathmacloudeqs@users.noreply.github.com>
2023-12-13 18:47:47 +0530

    Update push-to-release.ps1

--- push-to-release.ps1
+++ push-to-release.ps1
@@ -20,5 +20,6 @@ git commit -m "Update release branch"
 
 # Push changes to the release branch using a personal access token (replace YOUR_TOKEN and YOUR_USERNAME)
 $token = "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"
-$githubUsername = "Anilathmacloudeqs"
-git push origin release -u $token:$githubUsername/release
+$githubUsername = "YOUR_USERNAME"
+git push origin release -u ($token + ':' + $githubUsername + '/release')
+
