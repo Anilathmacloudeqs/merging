073684956ac21e0aea079826d5d8fd231aa929d3
Anilathmacloudeqs <129284639+Anilathmacloudeqs@users.noreply.github.com>
2023-12-13 16:22:23 +0530

    Update push-to-release.ps1

--- push-to-release.ps1
+++ push-to-release.ps1
@@ -1,27 +1,28 @@
-# Define variables
-$sourceBranch = "main"
-$destinationBranch = "release"
-$fileToPush = "hello.py"
-
-# Print statement
-Write-Host "test1"
-
-# Set Git user information
-git config --global user.email "actions@github.com"
-git config --global user.name "GitHub Actions"
-
-# Fetch the release branch (create it if it doesn't exist)
-git fetch origin "$destinationBranch:$destinationBranch"
-
-# Checkout the release branch
-git checkout $destinationBranch
-
-# Copy the file from main branch to release branch
-git checkout $sourceBranch -- $fileToPush
-
-# Stage and commit changes
-git add $fileToPush
-git commit -m "Update $fileToPush from $sourceBranch to $destinationBranch"
-
-# Push changes to the release branch using the personal access token
-git push origin $destinationBranch
+- name: Execute PowerShell Script
+  shell: pwsh
+  run: |
+    $sourceBranch = "main"
+    $destinationBranch = "release"
+    $fileToPush = "hello.py"
+    
+    # Set Git user information
+    git config --global user.email "actions@github.com"
+    git config --global user.name "GitHub Actions"
+    
+    # Fetch the release branch (create it if it doesn't exist)
+    git fetch origin "${destinationBranch}:${destinationBranch}"
+    
+    # Checkout the release branch
+    git checkout $destinationBranch
+    
+    # Copy the file from main branch to release branch
+    git checkout $sourceBranch -- $fileToPush
+    
+    # Stage and commit changes
+    git add $fileToPush
+    git commit -m "Update $fileToPush from $sourceBranch to $destinationBranch"
+    
+    # Push changes to the release branch using the personal access token
+    git push origin $destinationBranch
+  env:
+    GITHUB_TOKEN: ${{ secrets.GH_TOKEN }}
