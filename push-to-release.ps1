2a3248d4e3f343d1098aa9571d4d11dd1c5bc589
Anilathmacloudeqs <129284639+Anilathmacloudeqs@users.noreply.github.com>
2023-12-13 16:38:39 +0530

    Update push-to-release.ps1

--- push-to-release.ps1
+++ push-to-release.ps1
@@ -1,1 +1,8 @@
-Write-Host "Hello"
+# push-to-release.ps1
+
+# Run the hello.py script
+$pythonScriptPath = ".\hello.py"
+$result = & python $pythonScriptPath
+
+# Print the result
+Write-Host "Result from hello.py script: $result"
